c8636808adf7a0fc843c78f8d55299ab2d591d69
Bdazzle <bprobins1013@gmail.com>
2025-04-20 21:00:52 -0400

    Update planarDeck.ts
    updated planar sets w/o phenomenons

--- functions/planarDeck.ts
+++ functions/planarDeck.ts
@@ -31,8 +31,8 @@ const generatePlanarDeck = (totalPlayers : number, planarData: PlaneChaseSet) =>
         return acc
     }, { planes: {}, phenomenon: {} } as PlanarDeck)
     const shuffledPlanes = shuffle(Object.entries(totalCards.planes))
-    const shuffledPhenom = shuffle(Object.entries(totalCards.phenomenon), totalPlayers * 2)
-    const randomizedCards = shuffle([...shuffledPlanes, ...shuffledPhenom])
+    const shuffledPhenom = Object.keys(totalCards.phenomenon).length && shuffle(Object.entries(totalCards.phenomenon), totalPlayers * 2)
+    const randomizedCards = shuffledPhenom ? shuffle([...shuffledPlanes, ...shuffledPhenom]) : shuffledPlanes
     const newDeck = randomizedCards.slice(0, totalPlayers * 10)
     return newDeck
 }
