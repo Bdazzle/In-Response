5b597a5facbab7ab47bba9c4b1d5a11edc001440
Bdazzle <bprobins1013@gmail.com>
2026-04-02 11:41:22 -0400

    storage
    cleared storage

--- OptionsContext.tsx
+++ OptionsContext.tsx
@@ -37,17 +37,17 @@ export const OptionsProvider: React.FC<{ children: React.ReactNode }> = ({ child
     /*
     clear storage
     */
-    useEffect(() => {
-        (async () =>{
-            try {
-                await AsyncStorage.clear()
-            }
-            catch(e){
-                console.log('error clearing storage', e)
-            }
-        })()
-        // getSavedData()
-    },[])
+    // useEffect(() => {
+    //     (async () =>{
+    //         try {
+    //             await AsyncStorage.clear()
+    //         }
+    //         catch(e){
+    //             console.log('error clearing storage', e)
+    //         }
+    //     })()
+    //     // getSavedData()
+    // },[])
 
     return <OptionsContext.Provider value={{
         deviceType,
